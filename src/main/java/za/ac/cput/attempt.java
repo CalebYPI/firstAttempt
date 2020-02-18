171475c7e742fad43f41b2f974da0c487cfdc71f
Caleb <caleb.ypi@gmail.com>
2020-02-18 23:01:27 +0200

    Multiplication added

---
+++ src/main/java/za/ac/cput/attempt.java
@@ -0,0 +1,7 @@
+package za.ac.cput;
+
+public class attempt {
+    public double multiplicity(int num, int demo) {
+        return num * demo;
+    }
+}
---
+++ src/test/java/za/ac/cput/attemptTest.java
@@ -0,0 +1,22 @@
+package za.ac.cput;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class attemptTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void multiplicity() {
+        Assert.assertNull(true);
+    }
+}
